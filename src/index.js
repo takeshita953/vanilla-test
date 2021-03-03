2f96dbd5e7cedc94a0980934355f6079630482a3
takeshita953 <79978458+takeshita953@users.noreply.github.com>
2021-03-04 08:52:21 +0900

    edit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-import "./styles.css";
+// var val1 = 'hello';
+// console.log(val1);
+// val1 = 'hello2';
+// console.log(val1);
+// var val1 = 'hoge';
+// console.log(val1);
+// let val2 = 'hello2';
+// console.log(val2);
+// val2 = 'hello2-1';
+// console.log(val2);
+// let val2 = 'hello2-2';
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// const val3 = 'hello3';
+// console.log(val3);
+// //val3 = 'hello3-1';
+// var val3='ho';
